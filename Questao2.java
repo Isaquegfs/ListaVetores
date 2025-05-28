99fb68497425de96bdfbef4592198b70150014a3
Isaque Gabriel <isaquegabriel8@hotmail.com>
2025-05-28 17:39:49 -0300

    Questao 2 (repetida em outra lista) adicionada

--- Questao2.java
+++ Questao2.java
@@ -2,8 +2,8 @@ package ListaVetores;
 
 public class Questao2 {
     public static void main(String[] args) throws Exception {
-
-
-        
+        for (int i = 0; i <= 100000; i = i + 1000){
+            System.out.println(i);
+        }
     }
 }
